6d050c1a1d37e21359dedce8e261ce6106de3ea9
LML <lilydragondrawer@gmail.com>
2018-03-01 11:43:06 -0500

    TESTING - added test code in main.js for user module

--- src/main.js
+++ src/main.js
@@ -19,3 +19,7 @@ app.listen(config.port, () => {
 	console.log(`listening on port ${config.port}`);
 	console.log(`    navigate to localhost:${config.port} for development`);
 });
+
+
+var User = require("./models/user.js");
+var usertest = new User({room_id: 1, name: "Joe", year: "freshman", major: "cs", topics: [1,2,3], auth_token_hash: "doot"});
