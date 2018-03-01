ee7757a1a91d95273fa06c56af626eed44bbe2f5
LML <lilydragondrawer@gmail.com>
2018-02-28 21:18:16 -0500

    added more detailed user schema

--- src/models/user.js
+++ src/models/user.js
@@ -5,6 +5,38 @@ class User {
 		return Promise.resolve([]);
 	}
 
+	const Model = sequelize.define('user', {
+		id: {
+			type: Sequelize.INTEGER,
+			primaryKey: true,
+			autoIncrement: true
+		},
+		room_id: {
+			type: Sequelize.INTEGER,
+			notNull: true
+		},
+		name: {
+			type: Sequelize.STRING,
+			notNull: true
+		},
+		year: {
+			type: Sequelize.STRING,
+			notNull: true
+		},
+		major: {
+			type: Sequelize.STRING,
+			notNull: true
+		},
+		topics: {
+			type: Sequelize.ARRAY(Sequelize.INTEGER),
+			notNull: true
+		},
+		auth_token_hash: {
+			type: Sequelize.STRING,
+			notNull: true
+		},
+	});
+
 	destroy () {}
 }
 
