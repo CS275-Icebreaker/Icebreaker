056e3af012b1555da2f5024793ac1132ba4bbc31
WFaustin <30529724+WFaustin@users.noreply.github.com>
2018-03-03 21:11:47 -0500

    Update main.js
    Updated main.js with endpoints for rooms/not finished

--- src/main.js
+++ src/main.js
@@ -3,16 +3,46 @@
 // App configuration
 const config = require("./config");
 
+// MongoDB setup
+const mongoose = require("mongoose");
+mongoose.connect(config.mongoURI);
+
 // Express setup
 const express = require("express");
 const app = express();
 
 app.use(express.static(config.staticDir));
 
+//Include Room module
+var room = require("./room"); 
+
 // Endpoints
 app.get("/api/test", (req, res) => {
 	res.send({status: "ok"});
 });
+
+//Creating room endpoint
+app.get("/api/creroom", (req, res) => {
+    //key is the data that will be queried from the ajax call
+    var code = req.query.key; 
+    var newroom = 
+
+});
+
+//Deleting room endpoint
+app.get("/api/delroom", (req, res) => {
+
+});
+
+//Find short code room endpoint
+app.get("/api/shortcode", (req, res) => {
+
+}); 
+
+//Get room info endpoint
+app.get("/api/roominfo", (req, res) => {
+
+}); 
 
 // Listen
 app.listen(config.port, () => {
