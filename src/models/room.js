f24f58aa7a355919d5f82d673041120c311eee12
pbb34 <pbb34@drexxel.edu>
2018-03-15 10:37:24 -0400

    Update room.js

--- src/models/room.js
+++ src/models/room.js
@@ -12,7 +12,7 @@ var RoomSchema = mongoose.Schema({
 	owner_id: mongoose.Schema.Types.ObjectId
 });
 
-var codeChars = "1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var codeChars = "1234567890";
 RoomSchema.statics.GenerateCode = function() {
 	return randtoken.generate(4, codeChars);
 };
