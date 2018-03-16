d1c62496e907bebeccc53e179b2f0e5aabc81d49
WFaustin <30529724+WFaustin@users.noreply.github.com>
2018-03-16 18:12:21 -0400

    Add files via upload

---
+++ src/endpoints/rooms/findRoom.js
@@ -0,0 +1,36 @@
+"use strict"; 
+
+var helpers = require("../helpers");
+var Room = require("../../models/room");
+var User = require("../../models/user");
+
+
+var URL = "/api/room/code/:code"; 
+
+
+function handle(req, res) {
+	var promise = Room.find({"code": req.params.code})
+	.then((room) => {
+		// If it doesn't exist
+		if (room == null) {
+			throw {msg: `room with id $(roomId) does not exist`, status: 404};
+		}
+		else {
+			return Promise.resolve(); 
+		}
+	})
+	.catch((err) => {
+		throw `error checking for room with provided id `; 
+	});
+	helpers.response.sendPromise(res, promise);	
+}
+
+
+
+function register(app){
+	app.get(URL, handle); 
+}
+
+module.exports = {
+    register: register
+}; 
