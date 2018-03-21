192ff1a9fb39d787a9f30e1a18862ce40e86e8a7
LML <lilydragondrawer@gmail.com>
2018-03-20 20:36:31 -0400

    Created seeder and data and tested functionality

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 
 var Server = require("./server");
 var db = require("./db");
+var Seeder = require("./seeders/topics.js");
 
 // Load configuration
 var config = require("./config");
@@ -17,6 +18,8 @@ db.connect()
 		console.error(`error connecting to MongoDB: ${err}`);
 		process.exit();
 	});
+
+Seeder.seedTopics();
 
 // Listen
 var server = new Server();
---
+++ src/seeders/topics.js
@@ -0,0 +1,29 @@
+"use strict"
+
+var Topic = require("./../models/topic.js");
+var topicData = require("./data/topics.json");
+
+function seedTopics() {
+	console.log("connected seeder");
+	var topics = topicData['topics'];
+	var promises = [];
+	
+	for(var num in topics) {
+		promises.push(Topic.findOneAndUpdate({name: topics[num]['name']}, {$set:{description: topics[num]['description'], picture_path: topics[num]['picture_path']}}, {new: true, upsert: true}, function(err, doc){
+			if(err){
+				console.log("Something wrong when updating data!");
+			}
+			//console.log(doc);
+		}));
+	}
+	
+	Promise.all(promises)
+		.then((topicModels) => {
+			console.log(`All ${topicModels.length} topics have been inserted`);
+		});
+}
+
+
+module.exports = {
+	seedTopics : seedTopics
+};
