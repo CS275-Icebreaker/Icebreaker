cc08d28699270f6ab0ebbda762dafefb45d5cbbb
Noah Huppert <contact@noahh.io>
2018-03-21 19:44:57 -0400

    made show room info screen

---
+++ src/www/js/create-room.js
@@ -0,0 +1,21 @@
+function createRoom() {
+    var roomName = document.getElementById("room-name").value;
+
+    var URL = "/api/rooms";
+
+    $.ajax({
+        type: "POST",
+        url: URL,
+        data: JSON.stringify({name: roomName}),
+        contentType: "application/json",
+        dataType: "html",
+        success: function(txt) {
+            window.room = JSON.parse(txt);
+
+            showRoom();
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log("Error fetching " + URL);
+        }
+    });
+}
---
+++ src/www/js/room-info.js
@@ -0,0 +1,4 @@
+function showRoom() {
+    document.getElementById("show-room-code").innerHTML = window.room.code;
+    document.getElementById("show-room-name").innerHTML = window.room.name;
+}
