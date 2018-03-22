ef757f3681eb1ab921c8d8a0e0f33b2cf1a31088
pbb34 <pbb34@drexxel.edu>
2018-03-22 11:05:05 -0400

    Revert "images and assigned topics update"
    This reverts commit d8528df480f3cae66d28b2eedd5662cc37fe200e.

--- src/www/js/index.js
+++ src/www/js/index.js
@@ -1,8 +1,23 @@
 //input validation stuff
+$(document).ready(function() {
+    $("#JoinForm").validate({
+        rules: {
+            SelectElmt: {
+                required: true
+            }
+        }
+    });
+});
 
 $(document).ready(function() {
             $("#JoinForm").validate();
-            $("#JoinForm2").validate();
+            $("#JoinForm2").validate({
+                rules: {
+                    room_key: {
+                        required: true
+                    }
+                }
+            });
             $("#JoinForm3").validate({
                 rules: {
                     SelectElmt: {
