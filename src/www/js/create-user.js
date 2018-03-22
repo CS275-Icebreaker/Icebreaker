aff1683f470c84c8eb5c923d1f56f931cd7bd267
LML <lilydragondrawer@gmail.com>
2018-03-21 21:30:49 -0400

    Finished create user call by hard coding in room_id

--- src/www/js/create-user.js
+++ src/www/js/create-user.js
@@ -1,20 +1,24 @@
-        function createUser()
-        {
-            var userName = document.getElementById("GetName").value;
-            var userYear = document.getElementById("GetName").value;
-            var userMajor = document.getElementById("GetName").value;
-            var URL = "http://localhost:8000/api/users/join";
-            $.ajax({
-                type: "POST",
-                url: URL,
-                data: JSON.stringify({name: roomName}),
-                contentType: "application/json",
-                dataType: "html",
-                success: function(msg) {
-                    console.log(msg);
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    console.log("Error fetching " + URL);
-                }
-            });
+function createUser()
+{
+    var room = "5ab2f51e2a620a16c439b96a";
+    var userName = document.getElementById("GetName").value;
+    var userYear = document.getElementById("GetYear").value;
+    var userMajor = document.getElementById("GetMajor").value;
+
+    var URL = "http://localhost:8000/api/users/join";
+
+    $.ajax({
+        type: "POST",
+        url: URL,
+        data: JSON.stringify({room_id: room, name: userName, year: userYear, major: userMajor}),
+        contentType: "application/json",
+        dataType: "html",
+        success: function(msg) {
+            console.log(msg);
+            window.user = msg;
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            console.log("Error fetching " + URL);
         }
+    });
+}
